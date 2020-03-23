b2d68f0d8bbe671499129538fa95509ecfc7a665
Diego Z <diego.zegarra78@gmail.com>
2020-03-23 18:51:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,33 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart (item) {
+  var price = Math.floor(100*(Math.random()));
+  var itemObj = {[item]: price};
+  cart.push(itemObj);
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
-
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } 
+  var newarray = [];
+  for(var i=0;i<cart.length;i++){
+    var keys = Object.keys(cart[i])[0]
+    newarray.push(keys + " at $" + cart[i][keys])
+  }
+  var mystring = "In your cart, you have "
+  if (newarray.length === 1){
+    mystring += newarray + "."
+  } else if (newarray.length === 2) {
+    mystring += (newarray[0] + " and " + newarray[1] + ".")
+  } else if (newarray.length > 2) {
+    var lastelement = newarray.pop()
+    var other_item = newarray.join(".")
+    mystring += (other_item + ", and" + lastelement + ".")
+  }
+  console.log(mystring)
 }
 
 function total() {
